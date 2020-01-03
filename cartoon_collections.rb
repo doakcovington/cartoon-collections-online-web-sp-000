7b41567830f42b8c1956b56c27df2a291b31188e
Doak Covington <doakcovington@protonmail.com>
2020-01-03 04:07:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese == cheese_types
+    cheese_types.include?(cheese)
   end
 end#end of find_the_cheese method
